be2b79e25c73243817b0cd2f5f6e98259dd3d48d
towdium <towdium@gmail.com>
2017-09-23 19:50:20 +0800

    Update deploy script

--- deploy_server.sh
+++ deploy_server.sh
@@ -4,12 +4,13 @@ set -x
 mkdir /tmp/deploy
 cd /tmp/deploy
 git clone https://github.com/Towdium/WebAPIs.git
-git clone https://github.com/Towdium/nginx-gunicorn.git
+git clone -b alpine-nginx-gunicorn https://github.com/Towdium/DockerImages.git
 rm -rf nginx-gunicorn/app
-mv WebAPIs nginx-gunicorn/app
-cd nginx-gunicorn/
+mv WebAPIs DockerImages/app
+cd DockerImages/
 docker build -t alpine-nginx-gunicorn .
 docker stop server
 docker rm server
 docker run -p 80:80 -d --restart unless-stopped --name server alpine-nginx-gunicorn
 rm -rf /tmp/deploy
+
